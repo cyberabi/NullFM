4cd484fd7a11d7f18da3b9981b589ad2ce0ee5b5
Christopher Burke <chris@playgigit.com>
2019-11-03 21:02:18 -0800

    Refactor and partial implementation of SPI device driver

--- asm.sh
+++ asm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export DEFSDIR=./defs
-export DEFSDIR2=/Users/cburke/Projects/OS9/cbos9/defs
-xasm -i=. -i=$DEFSDIR -i=$DEFSDIR2 $1
+export DEFSDIR2=/Volumes/Projects/OS9/cbos9/defs
+export DEFSDIR3=~/Projects/OS9/cbos9/defs
+xasm -i=. -i=$DEFSDIR -i=$DEFSDIR2 -i=$DEFSDIR3 $1
