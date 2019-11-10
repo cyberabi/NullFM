d2e12efb64e0dd506caa5e4207d666194563094e
Christopher Burke <burkechrisburke@gmail.com>
2019-11-09 19:46:05 -0800

    Added assembly control files and output generation

--- asm.sh
+++ asm.sh
@@ -2,4 +2,4 @@
 export DEFSDIR=./defs
 export DEFSDIR2=/Volumes/Projects/OS9/cbos9/defs
 export DEFSDIR3=~/Projects/OS9/cbos9/defs
-xasm -i=. -i=$DEFSDIR -i=$DEFSDIR2 -i=$DEFSDIR3 $1
+xasm -i=. -i=$DEFSDIR -i=$DEFSDIR2 -i=$DEFSDIR3 -o=modules/$(basename $1) $1
